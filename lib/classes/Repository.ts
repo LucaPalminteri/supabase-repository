1aa55f2cb374f4224ccc60e5ba8a7ccab7445fba
LucaPalminteri <lucapalminteri02@gmail.com>
2023-11-30 11:52:21 -0300

    add docs

--- lib/classes/Repository.ts
+++ lib/classes/Repository.ts
@@ -1,17 +1,46 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { IRepository } from "../interfaces/IRepository";
 
+/**
+ * Represents a repository for interacting with data stored in Supabase.
+ * Implements the IRepository interface to define common data access methods.
+ * 
+ * @class
+ * @implements {IRepository<any>}
+ */
 class Repository implements IRepository<any> {
 
     private _supabaseClient: SupabaseClient;
     private _tableName: string;
 
+    /**
+     * Creates an instance of Repository.
+     * 
+     * @constructor
+     * @param {SupabaseClient} supabaseClient - The Supabase client for data interaction.
+     * @param {string} tableName - The name of the table to interact with.
+     */
     constructor(supabaseClient: SupabaseClient, tableName: string) {
         this._supabaseClient = supabaseClient;
         this._tableName = tableName;
     }
 
-    public async GetAll() {
+    /**
+     * Retrieves all records from the specified table in Supabase.
+     *
+     * @returns {Promise<any[]>} A promise that resolves to an array of records.
+     * @throws {Error} If an error occurs during the data retrieval process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const allRecords = await yourInstance.GetAll();
+     *   console.log('All Records:', allRecords);
+     * } catch (error) {
+     *   console.error('Error getting all records:', error.message);
+     * }
+     */
+    public async GetAll(): Promise<any[]> {
         try {
             const { data, error } = await this._supabaseClient
                 .from(this._tableName)
@@ -26,6 +55,23 @@ class Repository implements IRepository<any> {
         }
     }
 
+    /**
+     * Retrieves a single record from the specified table in Supabase based on the provided ID.
+     *
+     * @param {number} id - The unique identifier of the record to retrieve.
+     * @returns {Promise<any | null>} A promise that resolves to the retrieved record, or `null` if no matching record is found.
+     * @throws {Error} If an error occurs during the data retrieval process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const recordId = 1;
+     *   const record = await yourInstance.GetById(recordId);
+     *   console.log('Record by ID:', record);
+     * } catch (error) {
+     *   console.error('Error getting record by ID:', error.message);
+     * }
+     */
     public async GetById(id: number): Promise<any | null> {
         try {
             const { data, error } = await this._supabaseClient
@@ -42,6 +88,23 @@ class Repository implements IRepository<any> {
         }
     }
 
+    /**
+     * Retrieves records from the specified table in Supabase based on an array of IDs.
+     *
+     * @param {number[]} ids - An array of unique identifiers of the records to retrieve.
+     * @returns {Promise<any[]>} A promise that resolves to an array of matching records.
+     * @throws {Error} If an error occurs during the data retrieval process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const recordIds = [1, 2, 3];
+     *   const records = await yourInstance.GetByIds(recordIds);
+     *   console.log('Records by IDs:', records);
+     * } catch (error) {
+     *   console.error('Error getting records by IDs:', error.message);
+     * }
+     */
     public async GetByIds(ids: number[]): Promise<any[]> {
         try {
             const { data, error } = await this._supabaseClient
@@ -59,6 +122,23 @@ class Repository implements IRepository<any> {
         }
     }
 
+    /**
+     * Inserts a new record into the specified table in Supabase.
+     *
+     * @param {any} object - The data object to insert.
+     * @returns {Promise<void>} A promise that resolves when the insertion is successful.
+     * @throws {Error} If an error occurs during the data insertion process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const newRecord = object;
+     *   await yourInstance.Insert(newRecord);
+     *   console.log('Record inserted successfully');
+     * } catch (error) {
+     *   console.error('Error inserting record:', error.message);
+     * }
+     */
     public async Insert(object: any): Promise<void> {
         try {
             const { data, error } = await this._supabaseClient
@@ -73,11 +153,30 @@ class Repository implements IRepository<any> {
         }
     }
 
-    public async Update(id: number, updatedany: any): Promise<void> {
+    /**
+     * Updates a record in the specified table in Supabase based on the provided ID.
+     *
+     * @param {number} id - The unique identifier of the record to update.
+     * @param {any} updatedObject - The updated data object.
+     * @returns {Promise<void>} A promise that resolves when the update is successful.
+     * @throws {Error} If an error occurs during the data update process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const recordId = 1;
+     *   const updatedData = object;
+     *   await yourInstance.Update(recordId, updatedData);
+     *   console.log('Record updated successfully');
+     * } catch (error) {
+     *   console.error('Error updating record:', error.message);
+     * }
+     */
+    public async Update(id: number, updatedObject: any): Promise<void> {
         try {
             const { data, error } = await this._supabaseClient
                 .from(this._tableName)
-                .update(updatedany)
+                .update(updatedObject)
                 .eq('id', id);
             if (error) {
                 throw new Error(`Error updating data: ${error.message}`);
@@ -89,6 +188,23 @@ class Repository implements IRepository<any> {
         }
     }
 
+    /**
+     * Deletes a record from the specified table in Supabase based on the provided ID.
+     *
+     * @param {number} id - The unique identifier of the record to delete.
+     * @returns {Promise<void>} A promise that resolves when the deletion is successful.
+     * @throws {Error} If an error occurs during the data deletion process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const recordId = 1;
+     *   await yourInstance.Delete(recordId);
+     *   console.log('Record deleted successfully');
+     * } catch (error) {
+     *   console.error('Error deleting record:', error.message);
+     * }
+     */
     public async Delete(id: number): Promise<void> {
         try {
             const { error } = await this._supabaseClient
@@ -104,6 +220,21 @@ class Repository implements IRepository<any> {
         }
     }
 
+    /**
+     * Retrieves the count of records in the specified table in Supabase.
+     *
+     * @returns {Promise<number>} A promise that resolves to the count of records.
+     * @throws {Error} If an error occurs during the data retrieval process.
+     *
+     * @example
+     * // Usage example:
+     * try {
+     *   const recordCount = await yourInstance.Count();
+     *   console.log('Record count:', recordCount);
+     * } catch (error) {
+     *   console.error('Error getting record count:', error.message);
+     * }
+     */
     public async Count(): Promise<number> {
         try {
             const { count, error } = await this._supabaseClient
